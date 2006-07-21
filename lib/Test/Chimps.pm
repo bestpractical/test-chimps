075826ae1c73ca862dc19106bf7698dbc9fad901
Zev Benjamin <zev@bestpractical.com>
2006-07-21 20:16:30 +0000

    * fixed dep and upped version number

--- lib/Test/Chimps.pm
+++ lib/Test/Chimps.pm
@@ -4,11 +4,11 @@ Test::Chimps - Collaborative Heterogeneous Infinite Monkey Perfectionification S
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
