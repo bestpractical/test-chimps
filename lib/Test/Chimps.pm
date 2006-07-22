0e6cc576560e111b92c4070ae9f37a8a8d5b5493
Zev Benjamin <zev@bestpractical.com>
2006-07-22 01:30:55 +0000

    * added dummy file to work around Module::Install bug r11834@truegrounds:  zev | 2006-07-21 21:30:47 -0400 * bumped version number for release

--- lib/Test/Chimps.pm
+++ lib/Test/Chimps.pm
@@ -4,11 +4,11 @@ Test::Chimps - Collaborative Heterogeneous Infinite Monkey Perfectionification S
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
